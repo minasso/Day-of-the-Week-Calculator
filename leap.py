6d33af6ab13f98f323d33f5e601cae3728caece1
minasso <andtas@gmail.com>
2017-05-29 03:24:50 -0400

    added leap module to make leap year detection more robust

---
+++ leap.py
@@ -0,0 +1,12 @@
+def isleap(year):
+    if year%400==0:
+        leap=True
+    elif year%100==0:
+        leap=False
+    elif year%4==0:
+        leap=True
+    else:
+        leap=False
+    print(leap)
+    return(leap)
+isleap(1900)
