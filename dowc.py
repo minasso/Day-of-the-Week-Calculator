60e2cc378dbe727435d687c3f5f850e45cd78817
minasso <andtas@gmail.com>
2017-05-29 20:45:38 -0400

    added in mutually exclusive group for quiet and verbose

--- dowc.py
+++ dowc.py
@@ -1,10 +1,16 @@
 import argparse
 import sys
-from leap import isleap
+# from leap import isleap
+from calendar import isleap
 import re
 
 parser = argparse.ArgumentParser(description= 'Pass in date in cmd line')
 parser.add_argument('-d', '--date', metavar='', type=str, help= 'date in mm/dd/yyyy')
+#add in mutually exclusive group for quiet vs verbose
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q,', '--quiet', action='store_true', help='print just result')
+group.add_argument('-v,', '--verbose', action='store_true', help='print addl info (out anchor, doomsday, etc)')
+args = parser.parse_args()
 
 def user_input():
     date = input('Enter a date between 1/1/1800 and 12/31/2199:    ')
@@ -31,13 +37,19 @@ def century(year):
         anchor = 0
     else:
         print('Error: Year must be between 1800 and 2199')
-    print('Anchor: ' + str(anchor))
+    # if args.quiet:
+    #     pass
+    # else:
+        # print('Anchor: ' + str(anchor))
     return(anchor)
 
         
 def year_(y, anchor):
     doomsday= (( y//12 + y%12 + (y%12)//4 ) % 7  + anchor )%7
-    print('Doomsday: ' +str(doomsday))
+    # if args.quiet:
+    #     pass
+    # else:
+        # print('Doomsday: ' +str(doomsday))
     return(doomsday)
    
 #so far, we have a number that corresponds to the century and then the year. 
@@ -54,7 +66,10 @@ def day_sub(month):
     l2 = [4,0,0,3,5,1,3,6,2,4,0,2]
     d = dict(zip(l1,l2))
     n = d[month]
-    print('Month Contibution: '+ str(n))
+    # if args.quiet:
+    #     pass
+    # else:
+    #     print('Month Contibution: '+ str(n))
     return(n)
 
 
@@ -66,8 +81,8 @@ def day_of_week(integer):
     return(codes[integer])
 
 def main():
-    if len(sys.argv) >1:
-        date = sys.argv[1]
+    if len(sys.argv) >1:     #means that user included the date in the cmd line 
+        date = args.date  
     else:
         date = user_input()
     month, day, year, y = date_parser(date)
@@ -79,21 +94,20 @@ def main():
         if month ==2 or 1:  #just changed to 1 from 3
             total = total-1
     day_ = day_of_week(total)
-    print('Day of the Week for {} is {}'.format(date,day_))
+    if args.quiet:
+        print(day_)
+    elif args.verbose:
+        print(date)
+        print('Century Contribution = Anchor: ' + str(anchor))
+        print('Year Contribution = Doomsday: ' +str(doomsday))
+        print('Month Contibution: '+ str(n))
+        print('Day of the Week for {} is: {}'.format(date,day_))
 
-d='12/31/1986'
-d1='3/13/2017'
-d2='4/1/2017'
-d3='2/29/2016'
-d4='11/30/1983'
+
+    else:
+        print('Day of the Week for {} is: {}'.format(date,day_))
+
 main()
-
-
-#output the anchor, 
-
-
-
-#code in leap year
 
 
 
